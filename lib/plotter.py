ad0a2e494205b1adbadfebfcfdcfcf78fc7ceac4
GertMadsen <cph-gm50@cphbusiness.dk>
2018-10-30 22:04:58 +0100

    new module type and easier DiGraph

--- lib/plotter.py
+++ lib/plotter.py
@@ -3,20 +3,23 @@ import matplotlib.pyplot as plt
 import os
 
 def make_graph(url_dict, plot_file):
-    plot_data = []   
-    for key, values in url_dict.items():
-        for value in values:
-            plot_data.append((key,value,1))      
+    # plot_data = []   
+    # for key, values in url_dict.items():
+    #     for value in values:
+    #         plot_data.append((key,value,1))      
     
-    DG = nx.DiGraph()
-    DG.add_weighted_edges_from(plot_data)
+    # DG = nx.DiGraph()
+    # DG.add_weighted_edges_from(plot_data)
+    
+    DG = nx.DiGraph(url_dict, rank="source")
     options = {
       'with_labels': False,  
       'node_color': 'black',
       'edge_color': 'grey',
       'node_size': 2,
       'width': 0.1,     
-      'arrows': False,
+      'arrows': True,
+      'arrowsize' : 5,
     }
     nx.draw(DG, **options)
 
--- main.py
+++ main.py
@@ -17,22 +17,34 @@ if __name__ == '__main__':
         _, url, depth, module_name = sys.argv
 
         plot_file = module_name+".png"
-        module_name += ".p"
+        module_name += ".py"
         
-
         print('Starting webscrape.')   
         result = webscraper.scrape_starter(url, int(depth))
+        print('Numbers of links pages scraped: {}'.format(len(result)))
         print('Webscraping finished succesfully.')
         
-        print('\nSaving result dictionary in a pickle module as: {}'.format(module_name))        
+        # print('\nSaving result dictionary in a pickle module as: {}'.format(module_name))        
+        # try:
+        #     module_dir = 'pickle_modules'
+        #     if not os.path.isdir(module_dir):
+        #         os.makedirs(module_dir)
+        #     with open(os.path.join(module_dir, module_name), "wb" ) as fp:
+        #         pickle.dump( result, fp)
+        #     print('Pickle module was saved succesfully.')
+        # except Exception as e:
+        #     print('Pickle module save failure!')
+
+        print('\nSaving result dictionary in a python module as: {}'.format(module_name)) 
         try:
-            module_dir = 'pickle_modules'
+            module_txt = 'SCRAPED_LINKS = {}'.format(str(result))
+            module_dir = 'modules'
             if not os.path.isdir(module_dir):
                 os.makedirs(module_dir)
-            pickle.dump( result, open( os.path.join(module_dir, module_name), "wb" ))
-            print('Pickle module was saved succesfully.')
+            with open(os.path.join(module_dir, module_name), "w" ) as fp:
+                fp.write(module_txt)
         except Exception as e:
-            print('Pickle module save failure!')
+            print('Python module save failure!')        
 
         print('\nCreating and viewing plot.')
         plotter.make_graph(result, plot_file)
